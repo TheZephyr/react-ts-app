58fe5974ba45b17cc15766de6accd10c419de650
TheZephyr <slodownik.maciej@gmail.com>
2020-06-11 23:04:24 +0200

    Add Image component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Image from "./components/Image";
 
 function App() {
   return (
     <div className="App">
       <div className="App-header">
-        <img src="img/img1.jpg" alt="Widoczek1">
-        </img>
+        <Image />       
       </div>
     </div>
   );
---
+++ src/components/Image.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Image() {
+    return <img src="img/img1.jpg" alt="Widoczek1"></img>;
+}
+
+export default Image;
